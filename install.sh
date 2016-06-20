072ecd278136355318c7a4bd65bd0f9c680cb19b
Marcello Mortaro <mortaromarcello@gmail.com>
2016-06-20 11:32:25 +0000

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-
+if [ $(id -u) != 0 ]; then
+  echo "$USER deve essere root"
+  exit
+fi
 apt-get install libc6-i386
 cp -va usr/* /usr/
 cp -va var/* /var/
 /usr/local/Brother/Printer/MFC7360N/cupswrapper/cupswrapperMFC7360N-2.0.4 -i
+service cups restart
